48ed1ab66e5d8aef4b852c1e8cd085eee36f359f
David Watts <davidwatts11@gmail.com>
2021-12-09 22:05:37 +1000

    currency formatting

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const stockData = []
 const ticker = document.querySelector("#ticker").value
+const quantity = document.querySelector("#quantity").value
 const api_token = "X3ZH5F4C7ZCUID7K"
+
+const formatter = new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: 'USD'
+})
 
 
 document.querySelector("button").addEventListener("click", function(e) {
@@ -9,12 +15,16 @@ document.querySelector("button").addEventListener("click", function(e) {
     $.getJSON(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${api_token}`, data => {
         stockData.push(data["Time Series (Daily)"])
         // console.log(stockData[0]['2021-12-08']['4. close'])
-        const el = document.getElementById("table")
+        const el = document.querySelector("table")
+        const price = stockData[0]['2021-12-08']['4. close']
+        const date = "2012-12-08"
         el.innerHTML += (
             `<tr> 
                 <td> ${ticker} </td> 
-                <td> ${stockData[0]['2021-12-08']['4. close']} </td> 
-                <td> 2021-12-08 </td> 
+                <td> ${formatter.format(price)} </td> 
+                <td> ${quantity} </td> 
+                <td> ${formatter.format(price * quantity)}</td> 
+                <td> ${date} </td> 
             </tr>`
         )
     })
