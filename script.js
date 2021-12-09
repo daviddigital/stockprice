0fe115bbe15b71a64e801b345fd26e7764c797a9
David Watts <davidwatts11@gmail.com>
2021-12-09 21:25:33 +1000

    added quantity

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const stockData = []
-const ticker = "IBM"
+const ticker = document.querySelector("#ticker").value
+const api_token = "X3ZH5F4C7ZCUID7K"
 
-document.getElementById("button").addEventListener("click", function(e) {
+
+document.querySelector("button").addEventListener("click", function(e) {
     console.log(e)
     e.preventDefault()
-    $.getJSON("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo", data => {
+    $.getJSON(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${api_token}`, data => {
         stockData.push(data["Time Series (Daily)"])
-        console.log(stockData[0]['2021-12-08']['4. close'])
+        // console.log(stockData[0]['2021-12-08']['4. close'])
         const el = document.getElementById("table")
-        el.innerHTML += `<tr> <td> ${ticker} </td> <td> ${stockData[0]['2021-12-08']['4. close']} </td> <td> 2021-12-08 </td> </tr>`
+        el.innerHTML += (
+            `<tr> 
+                <td> ${ticker} </td> 
+                <td> ${stockData[0]['2021-12-08']['4. close']} </td> 
+                <td> 2021-12-08 </td> 
+            </tr>`
+        )
     })
   })
